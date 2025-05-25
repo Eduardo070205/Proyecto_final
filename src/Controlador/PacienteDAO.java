089c4968e362b71ab730b5ab7819542cbc9893c5
eduar <eduardo.dc@tecjerez.edu.mx>
2025-05-24 21:08:10 -0600

    Se creo el controlador PacienteDAO y se agrego el metodo agregarPacientes

--- src/Controlador/PacienteDAO.java
+++ src/Controlador/PacienteDAO.java
@@ -1,4 +1,25 @@
 package Controlador;
 
+import ConexionBD.ConexionBD;
+import Modelo.Paciente;
+
+import java.sql.Connection;
+
 public class PacienteDAO {
+
+    ConexionBD con = ConexionBD.getInstancia();
+
+
+    //=========================== ALTAS ============================
+
+
+    public boolean agregarPaciente(Paciente paciente){
+        
+        String sql = "INSERT INTO Pacientes VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+        return con.ejecutarInstruccionLMD(sql, paciente.getNumPaciente(), paciente.getNombre(), paciente.getApePat(), paciente.getApeMat(), paciente.getCalleNumero(), paciente.getColonia(), paciente.getCp(), paciente.getEstado(), paciente.getTelefono(), paciente.getFechaNac(), paciente.getSexo(), paciente.getEstadoCivil(), paciente.getFechaIngreso());
+
+    }
+
+
 }
